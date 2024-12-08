a43b7aade4cc0c6d21235c92a4b74cc5935e4279
JayAguilar916 <jay.aguilar916@gmail.com>
2024-12-08 01:57:39 -0500

    Added the create account functionality for Admin.

--- CommunityExchange/src/main/java/com/spring/jpa/model/User.java
+++ CommunityExchange/src/main/java/com/spring/jpa/model/User.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -12,6 +14,7 @@ import jakarta.persistence.Table;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // Auto-generate the userId
     @Column(name = "user_id")  // Change this to 'user_id' instead of 'username'
     private Long userId;  // Make this the primary key
     
